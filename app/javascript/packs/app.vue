9ad6f367bc0239448cd0aeaf3fb0231e4a5632cf
Konstantine <konstantinev@protonmail.com>
2020-07-30 20:45:40 -0500

    loading indicator works now

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -1,16 +1,22 @@
 <template>
 	<div id="app">
-		<pulse-loader v-if="loading"></pulse-loader>
-		<div>
-			<h2>{{ item.name }}</h2>
-			<img :src="item.image_url" :alt="item.name" />
+		<div v-if="loading">
+			<pulse-loader></pulse-loader>
+			<p>Loading...</p>
+		 <i>Hint: The loading is delayed</i>
 		</div>
-		<div>
-			<span>Click URL:</span> 
-			<a :href="item.click_url">visit product page</a>
-		</div>
-		<div>
-			<span>Price: ${{ item.shipping_and_handling.price }}</span>
+		<div v-else>
+			<div>
+				<h2>{{ item.name }}</h2>
+				<img :src="item.image_url" :alt="item.name" />
+			</div>
+			<div>
+				<span>Click URL:</span> 
+				<a :href="item.click_url">visit product page</a>
+			</div>
+			<div>
+				<span>Price: ${{ item.shipping_and_handling.price }}</span>
+			</div>
 		</div>
 	</div>
 </template>
@@ -33,7 +39,7 @@
 			return {
 				message: "Hello Vue!",
 				item: [],
-				loading: false,
+				loading: true,
 			}
 		},
 		methods: {
@@ -48,22 +54,24 @@
 			loadStop() {
 				this.loading = false
 			},
-			async makeRequest() {
-					this.loadStart()
+			makeRequest() {
 				axios.get(encodeURI(this.createUri())).then(r => (
-					this.item = r.data.data,
-					this.loadStop()
+					this.item = r.data.data
 				)).catch(err => {
 					(error) => {
-						this.state.loading = false,
-						this.loadStop()
+						this.state.loading = false
 						console.log(error)
 					}
 				})
 			}
 		},
 		mounted() {
+				this.loading = true
+			setTimeout(() => {
 				this.makeRequest()
+				console.log("niggerfaggot")
+			}, 2500);
+				this.loadStop()
 		}
 }
 </script>
